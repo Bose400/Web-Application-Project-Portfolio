3e87a4392f82acca35e7541d1446e0b6d196b21f
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2024-05-16 22:43:55 +0100

    My First Commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,14 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'In the realm of modern web development, crafting seamless user experiences and intuitive interfaces is paramount. As a passionate UI/UX developer, I am dedicated to bridging the gap between functionality and aesthetics, ensuring that every interaction with a digital product is not only efficient but also visually engaging. My expertise lies in conceptualizing and implementing user-centric designs that resonate with target audiences, fostering deeper connections and enhancing brand loyalty.',
+	},
+	{
+		id: 2,
+		bio: 'With proficiency in ReactJS, I bring dynamic and responsive web applications to life. Leveraging the power of this versatile JavaScript library, I architect scalable solutions that adapt effortlessly to varying screen sizes and device types. ReactJS empowers me to craft interactive interfaces with reusable components, streamlining development workflows and fostering code maintainability. Through meticulous attention to detail and a deep understanding of user behavior, I strive to elevate the user experience to new heights, one component at a time.',
+	},
+	{
+		id: 3,
+		bio: 'In the realm of frontend styling, Tailwind CSS stands out as my go-to tool for crafting visually stunning and highly customizable interfaces. Its utility-first approach allows me to rapidly prototype designs and iterate with ease, empowering creativity without sacrificing efficiency. From intricate layouts to pixel-perfect typography, Tailwind CSS enables me to translate design concepts into tangible digital experiences that captivate and inspire. With a keen eye for design trends and a passion for innovation, I leverage Tailwind CSS to create immersive and memorable user interfaces that leave a lasting impression.'
+	}
+];
---
+++ src/data/clientsData.js
@@ -0,0 +1,30 @@
+// Import images
+import AmazonImage from '../images/brands/amazon_gray.png';
+import SonyImage from '../images/brands/sony_gray.png';
+import AdidasImage from '../images/brands/adidas_gray.png';
+import AbimbolaImage from '../images/brands/logo.png';
+
+export const clientsHeading = 'Some of the brands that trust me';
+
+export const clientsData = [
+	{
+		id: 1,
+		title: 'Amazon',
+		img: AmazonImage,
+	},
+	{
+		id: 2,
+		title: 'Sony',
+		img: SonyImage,
+	},
+	{
+		id: 3,
+		title: 'Adidas',
+		img: AdidasImage,
+	},
+	{
+		id: 4,
+		title: 'Abimbola',
+		img: AbimbolaImage,
+	}
+];
